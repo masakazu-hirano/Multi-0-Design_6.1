c2e468497fd7a5e8654fcd7cd1dcb38afb144d0c
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-06 23:37:52 +0900

    User モデルにバリデーションを設定する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :agreement, acceptance: true
 end
