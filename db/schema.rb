28cba2392861428b87fa5fff7311925b1b372a03
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-07 00:31:04 +0900

    users テーブルにバリデーションを設定する。

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2021_01_05_142237) do
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+  create_table "users", id: { type: :integer, limit: 2 }, charset: "utf8", force: :cascade do |t|
+    t.string "name", limit: 255, null: false, collation: "utf8mb4_general_ci"
+    t.string "email", limit: 255, null: false
+    t.string "encrypted_password", limit: 255, null: false
+    t.string "reset_password_token", collation: "utf8mb4_general_ci"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
