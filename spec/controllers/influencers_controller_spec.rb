c63fa2d9b4b5d28d9d03aefcbfff6f544cc6f17e
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-07 22:19:37 +0900

    InfluencersController の Index アクションのテストコードを修正する。

--- spec/controllers/influencers_controller_spec.rb
+++ spec/controllers/influencers_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe InfluencersController do
   describe 'GET #index' do
     it 'index.html.erb にページ遷移する。' do
+      get :index
+      expect(response).to render_template :index
     end
   end
 end
