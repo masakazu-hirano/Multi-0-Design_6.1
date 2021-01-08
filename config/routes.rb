e3dbe8dac0ea7d9422c3e1db8120be102e1b0a8f
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-09 00:51:00 +0900

    採用ご担当者様用のログイン機能のルーティングを追加する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/recruiter_sign_in', to: 'users/recruiter_sessions#recruiter_login'
+  end
   root to: 'influencers#index'
 
   resources :influencers, only: :index
+  resources :youtube_videos, only: :index
 end
