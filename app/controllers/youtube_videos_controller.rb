aaa4fb3f5d8e893326962848ef1a8ff1364cb4b8
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-10 15:57:41 +0900

    動画検索をする際にチャンネル名を取得できるようにする。

--- app/controllers/youtube_videos_controller.rb
+++ app/controllers/youtube_videos_controller.rb
@@ -3,16 +3,19 @@ class YoutubeVideosController < ApplicationController
   end
 
   def new
+    session[:video_search_count] = params[:count].to_i
+
     require 'google/apis/youtube_v3'
     youtube = Google::Apis::YoutubeV3::YouTubeService.new
     youtube.key = Rails.application.credentials.YouTube[:Data_API_Key]
-    options = { q: params[:search] , order: 'relevance', max_results: params[:count].to_i }
+    options = { q: params[:search] , order: 'relevance', max_results: session[:video_search_count] }
     video_lists = youtube.list_searches(:snippet, options).to_h
 
     if params[:count].to_i == 1
       @video_lists_1 = Hash.new
       @video_lists_1[:video_id] = video_lists[:items][0][:id][:video_id]
       @video_lists_1[:title] = video_lists[:items][0][:snippet][:title]
+      @video_lists_1[:channel] = video_lists[:items][0][:snippet][:channel_title]
       @video_lists_1[:thumbnail] = video_lists[:items][0][:snippet][:thumbnails][:high][:url]
       @video_lists_1[:release_date] = video_lists[:items][0][:snippet][:published_at]
       session[:video_lists_1] = @video_lists_1
@@ -21,6 +24,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_1 = Hash.new
       @video_lists_1[:video_id] = video_lists[:items][0][:id][:video_id]
       @video_lists_1[:title] = video_lists[:items][0][:snippet][:title]
+      @video_lists_1[:channel] = video_lists[:items][0][:snippet][:channel_title]
       @video_lists_1[:thumbnail] = video_lists[:items][0][:snippet][:thumbnails][:high][:url]
       @video_lists_1[:release_date] = video_lists[:items][0][:snippet][:published_at]
       session[:video_lists_1] = @video_lists_1
@@ -28,6 +32,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_2 = Hash.new
       @video_lists_2[:video_id] = video_lists[:items][1][:id][:video_id]
       @video_lists_2[:title] = video_lists[:items][1][:snippet][:title]
+      @video_lists_2[:channel] = video_lists[:items][1][:snippet][:channel_title]
       @video_lists_2[:thumbnail] = video_lists[:items][1][:snippet][:thumbnails][:high][:url]
       @video_lists_2[:release_date] = video_lists[:items][1][:snippet][:published_at]
       session[:video_lists_2] = @video_lists_2
@@ -35,6 +40,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_3 = Hash.new
       @video_lists_3[:video_id] = video_lists[:items][2][:id][:video_id]
       @video_lists_3[:title] = video_lists[:items][2][:snippet][:title]
+      @video_lists_3[:channel] = video_lists[:items][2][:snippet][:channel_title]
       @video_lists_3[:thumbnail] = video_lists[:items][2][:snippet][:thumbnails][:high][:url]
       @video_lists_3[:release_date] = video_lists[:items][2][:snippet][:published_at]
       session[:video_lists_3] = @video_lists_3
@@ -43,6 +49,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_1 = Hash.new
       @video_lists_1[:video_id] = video_lists[:items][0][:id][:video_id]
       @video_lists_1[:title] = video_lists[:items][0][:snippet][:title]
+      @video_lists_1[:channel] = video_lists[:items][0][:snippet][:channel_title]
       @video_lists_1[:thumbnail] = video_lists[:items][0][:snippet][:thumbnails][:high][:url]
       @video_lists_1[:release_date] = video_lists[:items][0][:snippet][:published_at]
       session[:video_lists_1] = @video_lists_1
@@ -50,6 +57,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_2 = Hash.new
       @video_lists_2[:video_id] = video_lists[:items][1][:id][:video_id]
       @video_lists_2[:title] = video_lists[:items][1][:snippet][:title]
+      @video_lists_2[:channel] = video_lists[:items][1][:snippet][:channel_title]
       @video_lists_2[:thumbnail] = video_lists[:items][1][:snippet][:thumbnails][:high][:url]
       @video_lists_2[:release_date] = video_lists[:items][1][:snippet][:published_at]
       session[:video_lists_2] = @video_lists_2
@@ -57,6 +65,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_3 = Hash.new
       @video_lists_3[:video_id] = video_lists[:items][2][:id][:video_id]
       @video_lists_3[:title] = video_lists[:items][2][:snippet][:title]
+      @video_lists_3[:channel] = video_lists[:items][2][:snippet][:channel_title]
       @video_lists_3[:thumbnail] = video_lists[:items][2][:snippet][:thumbnails][:high][:url]
       @video_lists_3[:release_date] = video_lists[:items][2][:snippet][:published_at]
       session[:video_lists_3] = @video_lists_3
@@ -64,6 +73,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_4 = Hash.new
       @video_lists_4[:video_id] = video_lists[:items][3][:id][:video_id]
       @video_lists_4[:title] = video_lists[:items][3][:snippet][:title]
+      @video_lists_4[:channel] = video_lists[:items][3][:snippet][:channel_title]
       @video_lists_4[:thumbnail] = video_lists[:items][3][:snippet][:thumbnails][:high][:url]
       @video_lists_4[:release_date] = video_lists[:items][3][:snippet][:published_at]
       session[:video_lists_4] = @video_lists_4
@@ -71,6 +81,7 @@ class YoutubeVideosController < ApplicationController
       @video_lists_5 = Hash.new
       @video_lists_5[:video_id] = video_lists[:items][4][:id][:video_id]
       @video_lists_5[:title] = video_lists[:items][4][:snippet][:title]
+      @video_lists_5[:channel] = video_lists[:items][4][:snippet][:channel_title]
       @video_lists_5[:thumbnail] = video_lists[:items][4][:snippet][:thumbnails][:high][:url]
       @video_lists_5[:release_date] = video_lists[:items][4][:snippet][:published_at]
       session[:video_lists_5] = @video_lists_5
