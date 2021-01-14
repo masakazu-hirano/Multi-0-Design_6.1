bff6d0e3fc535d51ce8c993f1b6b4ed49e83bab3
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-14 20:56:32 +0900

    User モデルを再設計する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8 }
   validates :agreement, acceptance: true
 
-  has_many :youtube_videos, through: :videos_users
+  has_many :youtube_videos, through: :like_videos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_154850) do
+ActiveRecord::Schema.define(version: 2021_01_13_100000) do
 
-  create_table "users", id: { type: :integer, limit: 2 }, charset: "utf8mb4", force: :cascade do |t|
-    t.string "name", limit: 255, null: false
-    t.string "email", limit: 255, null: false, collation: "utf8_general_ci"
-    t.string "encrypted_password", limit: 255, null: false, collation: "utf8_general_ci"
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "videos_users", id: false, charset: "utf8mb4", force: :cascade do |t|
-    t.bigint "youtube_video_id"
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_videos_users_on_user_id"
-    t.index ["youtube_video_id"], name: "index_videos_users_on_youtube_video_id"
-  end
-
-  create_table "youtube_videos", charset: "utf8mb4", force: :cascade do |t|
-    t.string "channel"
-    t.string "video_id"
-    t.string "title"
-    t.binary "thumbnail"
-    t.date "release_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
