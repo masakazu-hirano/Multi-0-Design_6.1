5769c7db2f553309deaca32159b352c67a4d1751
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-15 19:25:58 +0900

    YouTube の動画情報を保存するテーブルを再設計する。

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -2,5 +2,5 @@ class YoutubeVideo < ApplicationRecord
   validates :channel, :video_id, :title, :thumbnail, :release_date, presence: true
   validates :video_id, uniqueness: true
 
-  has_many :users, through: :videos_users
+  has_many :users, through: :like_videos
 end
