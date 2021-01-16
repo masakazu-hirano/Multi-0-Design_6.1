f7feca0a5f9bdfbbadde453dc4f47bdb0057fa52
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-16 17:22:22 +0900

    トップページにブックマークの多い順で YouTube 動画を読み込む。

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -1,5 +1,6 @@
 class InfluencersController < ApplicationController
   def index
     @welcome_time = Time.now.strftime('%H').to_i
+    @video_likes = YoutubeVideo.includes(:like_videos).sort {|a,b| b.like_videos.size <=> a.like_videos.size}
   end
 end
