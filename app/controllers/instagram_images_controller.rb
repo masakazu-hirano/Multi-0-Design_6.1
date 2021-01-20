e0341349fb5dc965c10eadd263a4e62b4652350b
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-01-20 20:10:15 +0900

    Instagram のハッシュタグ検索から 'media_type' の値を取得するように編集する。

--- app/controllers/instagram_images_controller.rb
+++ app/controllers/instagram_images_controller.rb
@@ -1,15 +1,19 @@
 class InstagramImagesController < ApplicationController
+  def index
+    @welcome_time = Time.now.strftime('%H').to_i
+  end
+
   def create
     # ↓ Instagram ハッシュタグ ノードID を取得する。 ↓
     business_id = Rails.application.credentials.Instagram[:Business_ID]
     access_token = Rails.application.credentials.Instagram[:Access_Token]
 
-    search_word = URI.encode_www_form(q: 'バルーンアンクルパンツ')
+    search_word = URI.encode_www_form(q: params[:search])
     tag_id_uri = URI.parse("https://graph.facebook.com/v9.0/ig_hashtag_search?user_id=#{business_id}&#{search_word}&access_token=#{access_token}")
     tag_id = JSON.parse(Net::HTTP.get_response(tag_id_uri).body)['data'][0]['id']
 
     # ↓ Instagram ハッシュタグ検索 ↓
-    search_uri = URI.parse("https://graph.facebook.com/v9.0/#{tag_id}/top_media?user_id=#{business_id}&fields=media_type,media_url,children{media_url},like_count,permalink,timestamp&limit=3&access_token=#{access_token}")
+    search_uri = URI.parse("https://graph.facebook.com/v9.0/#{tag_id}/top_media?user_id=#{business_id}&fields=media_type,media_url,children{media_url},like_count,permalink,timestamp&limit=#{params[:count].to_i}&access_token=#{access_token}")
     search_results = JSON.parse(Net::HTTP.get_response(search_uri).body)
 
     image_lists = []
@@ -17,6 +21,7 @@ class InstagramImagesController < ApplicationController
       image_list = Hash.new
 
       if search_result['media_type'] == 'IMAGE'
+        image_list[:media_type] = search_result['media_type']
         image_list[:media_url] = search_result['media_url']
         image_list[:like_count] = search_result['like_count']
         image_list[:permalink] = search_result['permalink']
@@ -24,6 +29,7 @@ class InstagramImagesController < ApplicationController
 
         image_lists << image_list
       elsif search_result['media_type'] == 'CAROUSEL_ALBUM'
+        image_list[:media_type] = search_result['media_type']
         image_list[:like_count] = search_result['like_count']
         image_list[:permalink] = search_result['permalink']
         image_list[:timestamp] = search_result['timestamp']
@@ -38,20 +44,9 @@ class InstagramImagesController < ApplicationController
       end
     end
 
-    session[:instagram_images] = nil
-    session[:instagram_images] = image_lists
-  end
+    Rails.cache.delete('instagram_images')
+    Rails.cache.write('instagram_images', image_lists, expired_in: 1.hour, compress: true)
 
-  def new
-    @welcome_time = Time.now.strftime('%H').to_i
+    render action: :index, locals: { image_lists: image_lists }
   end
 end
-
-# Rails.application.credentials.Instagram[:Business_ID]
-# Rails.application.credentials.Instagram[:Access_Token]
-
-# ↓ Instagram ハッシュタグ ノードID を取得する。 ↓
-# JSON.parse(Net::HTTP.get_response(tag_id).body)
-
-# ↓ Instagram ハッシュタグ検索 ↓
-# JSON.parse(Net::HTTP.get_response(URI.parse("https://graph.facebook.com/v9.0/17885277574122814/top_media?user_id=17841444225960657&fields=id,like_count,media_url,permalink,timestamp&limit=3&access_token=#{Rails.application.credentials.Instagram[:Access_Token]}")).body)
