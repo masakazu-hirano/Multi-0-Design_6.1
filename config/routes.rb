aa63800e80b2730738a9a4c83a4629d072fbf1a3
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-03-26 13:28:15 +0900

    ユーザー管理機能のルーティングを編集する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,27 @@
 Rails.application.routes.draw do
   root to: 'roots#index'
 
-  # namespace :staff do
-  #   root 'top#index'
+  resources :users, only: [:new, :create]
+
+
+
+
+
+
+
+
+
+
+
+  # devise_for :users
+  # devise_scope :user do
+  #   get 'users/recruiter_sign_in', to: 'users/recruiter_sessions#recruiter_login'
+  #   get 'users/sign_out', to: 'devise/sessions#destroy'
   # end
 
 
-
-
-
-
-
-
-
-
-
-  devise_for :users
-  devise_scope :user do
-    get 'users/recruiter_sign_in', to: 'users/recruiter_sessions#recruiter_login'
-    get 'users/sign_out', to: 'devise/sessions#destroy'
-  end
-
-
-  resources :influencers, only: :index
-  resources :instagram_images, only: [:index, :create]
-  resources :youtube_videos, only: [:index, :create, :show]
-  resources :video_likes, only: [:create, :destroy]
+  # resources :influencers, only: :index
+  # resources :instagram_images, only: [:index, :create]
+  # resources :youtube_videos, only: [:index, :create, :show]
+  # resources :video_likes, only: [:create, :destroy]
 end
