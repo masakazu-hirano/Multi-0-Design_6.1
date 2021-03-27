2b62d9ea5ac3fd85140f45657958219e9f4ffe24
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-03-27 21:11:19 +0900

    MySQL に接続するための処理を修正する。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   def mysql_connect
     require 'mysql2'
-
-    @client = Mysql2::Client.new(host: :'db', username: :'root', password: ENV['MYSQL_ROOT_PASSWORD'], database: :'Multi_0_Design_development')
+    Mysql2::Client.new(host: :'db', username: :'root', password: ENV['MYSQL_ROOT_PASSWORD'], database: :'Multi_0_Design_development')
   end
 end
