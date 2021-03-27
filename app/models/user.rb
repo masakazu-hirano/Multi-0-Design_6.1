3cd9f6bd25c8d51c3d7ecb96c0b27d86657f1538
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-03-27 19:08:07 +0900

    ユーザー情報を新規登録する際のバリデーションを記述する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
+
+  def regist_check
+    @error = Array.new
+
+      if self.email == ''
+        @error << 'メールアドレスが未入力です。'
+      elsif self.email.include?('@') == false
+        @error << 'メールアドレスを正しく入力してください。'
+      end
+
+      if self.password == ''
+        @error << 'パスワードが未入力です'
+      elsif self.password.length < 8
+        @error << 'パスワードを 8文字以上で入力してください。'
+      end
+
+    return @error
+  end
 end
