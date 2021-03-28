9ffecdc03a3cae0b5b463a1bcffa1865a76b2ad5
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-03-28 18:46:40 +0900

    トップページでログイン状態を判別する処理を記述する。

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,4 +1,7 @@
 class RootsController < ApplicationController
   def index
+    unless session[:user_id].nil?
+      @user = User.find_by_sql("SELECT id, email, last_login FROM users WHERE id = #{session[:user_id]};")[0]
+    end
   end
 end
